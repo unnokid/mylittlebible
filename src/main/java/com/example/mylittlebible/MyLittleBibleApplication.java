ea951f2eb207af578591b0ab6ea1c141eb88cd78
unnokid <kimki0609@gmail.com>
2022-10-26 16:56:22 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/mylittlebible/MyLittleBibleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylittlebible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleBibleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyLittleBibleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mylittlebible/MyLittleBibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mylittlebible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLittleBibleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
