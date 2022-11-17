331617cad9c8d6182550328404352fabc00e87b0
unnokid <kimki0609@gmail.com>
2022-11-17 20:48:33 +0900

    feat : Bible 클래스 추가

---
+++ src/main/java/com/example/mylittlebible/domain/bible/entity/Bible.java
@@ -0,0 +1,28 @@
+package com.example.mylittlebible.domain.bible.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("bible")
+@Getter
+@Setter
+public class Bible {
+
+  private String _id;
+
+  private String title;
+
+  private Long chapter;
+
+  private Long verse;
+
+  private String content;
+
+  public Bible(String title, Long chapter, Long verse, String content) {
+    this.title = title;
+    this.chapter = chapter;
+    this.verse = verse;
+    this.content = content;
+  }
+}
