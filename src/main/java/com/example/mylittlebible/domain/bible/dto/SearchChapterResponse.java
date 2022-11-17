040ec0c69c35cd5f80d2432e571e323782a2b985
unnokid <kimki0609@gmail.com>
2022-11-17 21:10:10 +0900

    feat : @Builder 추가

--- src/main/java/com/example/mylittlebible/domain/bible/dto/SearchChapterResponse.java
+++ src/main/java/com/example/mylittlebible/domain/bible/dto/SearchChapterResponse.java
@@ -2,12 +2,14 @@ package com.example.mylittlebible.domain.bible.dto;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SearchChapterResponse {
   List<BibleDto> list;
 }
