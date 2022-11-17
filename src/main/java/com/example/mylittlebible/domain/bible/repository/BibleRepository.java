00ef6ea2f679238a2b6723ac8d308cfa9e534820
unnokid <kimki0609@gmail.com>
2022-11-17 20:48:56 +0900

    feat : BibleRepository 추가

---
+++ src/main/java/com/example/mylittlebible/domain/bible/repository/BibleRepository.java
@@ -0,0 +1,12 @@
+package com.example.mylittlebible.domain.bible.repository;
+
+import com.example.mylittlebible.domain.bible.entity.Bible;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BibleRepository extends MongoRepository<Bible, String> {
+  Optional<Bible> findBibleByTitleAndChapterAndVerse(String title, Long chapter, Long verse);
+  List<Bible> findAllByTitleAndChapter(String title, Long chapter);
+  List<Bible> findAllByTitle(String title);
+}
