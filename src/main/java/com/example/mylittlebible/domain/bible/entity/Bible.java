ddb22e57f19ff4a29bafb2069d0e2e40b51481d1
unnokid <kimki0609@gmail.com>
2022-11-23 21:13:53 +0900

    feat: bible 생성자생성시 경계값 처리 추가

--- src/main/java/com/example/mylittlebible/domain/bible/entity/Bible.java
+++ src/main/java/com/example/mylittlebible/domain/bible/entity/Bible.java
@@ -1,12 +1,11 @@
 package com.example.mylittlebible.domain.bible.entity;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
+import static org.apache.logging.log4j.util.Strings.isBlank;
 
 @Document("bible")
 @Getter
-@Setter
 public class Bible {
 
     private String _id;
@@ -20,9 +19,37 @@ public class Bible {
     private String content;
 
     public Bible(String title, Long chapter, Long verse, String content) {
+        setTitle(title);
+        setChapter(chapter);
+        setVerse(verse);
+        setContent(content);
+    }
+
+    private void setTitle(String title){
+        checkBlank(title);
         this.title = title;
+    }
+    private void setChapter(Long chapter){
+        checkNumber(chapter);
         this.chapter = chapter;
+    }
+    private void setVerse(Long verse){
+        checkNumber(verse);
         this.verse = verse;
+    }
+    private void setContent(String content){
+        checkBlank(content);
         this.content = content;
+    }
+
+    private void checkBlank(String s){
+        if(isBlank(s)){
+            throw new RuntimeException();
+        }
+    }
+    private void checkNumber(Long number){
+        if(number<=0){
+            throw new RuntimeException();
+        }
     }
 }
