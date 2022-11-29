b20329326128b5225bf9795d38fac53e2552d8ad
unnokid <kimki0609@gmail.com>
2022-11-29 21:36:03 +0900

    feat: Repository에 QuerydslPredicateExecutor 추가

--- src/main/java/com/example/mylittlebible/domain/bible/repository/BibleRepository.java
+++ src/main/java/com/example/mylittlebible/domain/bible/repository/BibleRepository.java
@@ -4,8 +4,9 @@ import com.example.mylittlebible.domain.bible.entity.Bible;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface BibleRepository extends MongoRepository<Bible, String> {
+public interface BibleRepository extends MongoRepository<Bible, String>, QuerydslPredicateExecutor<Bible> {
 
     Optional<Bible> findBibleByBookAndChapterAndVerse(String book, Long chapter, Long verse);
     List<Bible> findBibleByBookAndChapter(String book, Long chapter);
