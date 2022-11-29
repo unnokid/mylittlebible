9630893c9118ec052abb9b55585483217e508e20
unnokid <kimki0609@gmail.com>
2022-11-29 21:04:06 +0900

    refactor: service에 메서드 이름을 get-> search로 변경

--- src/main/java/com/example/mylittlebible/domain/bible/service/BibleService.java
+++ src/main/java/com/example/mylittlebible/domain/bible/service/BibleService.java
@@ -1,9 +1,7 @@
 package com.example.mylittlebible.domain.bible.service;
 
 import com.example.mylittlebible.domain.bible.dto.BibleDto;
-import com.example.mylittlebible.domain.bible.dto.SearchBookResponse;
-import com.example.mylittlebible.domain.bible.dto.SearchChapterResponse;
-import com.example.mylittlebible.domain.bible.dto.SearchSectionResponse;
+import com.example.mylittlebible.domain.bible.dto.SearchResponse;
 import com.example.mylittlebible.domain.bible.entity.Bible;
 import com.example.mylittlebible.domain.bible.repository.BibleRepository;
 import com.example.mylittlebible.domain.bible.util.BibleConverter;
@@ -19,46 +17,21 @@ public class BibleService {
         this.bibleRepository = bibleRepository;
     }
 
-
-    //타이틀 찾기
-    public SearchBookResponse getBook(String book) {
+    public SearchResponse searchBook(String book) {
         List<Bible> list = bibleRepository.findBibleByBook(book);
-        return BibleConverter.bookFromBible(list);
+        return BibleConverter.toSearchResponse(list);
     }
 
-    //장으로 찾기
-    public SearchChapterResponse getChapter(String book, Long chapter) {
+    public SearchResponse searchChapter(String book, Long chapter) {
         List<Bible> list = bibleRepository.findBibleByBookAndChapter(book, chapter);
-        return BibleConverter.chapterFromBible(list);
+        return BibleConverter.toSearchResponse(list);
     }
 
-    //특정 찾기
-    public BibleDto getVerse(String book, Long chapter, Long verse) {
+    public BibleDto searchVerse(String book, Long chapter, Long verse) {
         Bible bible = bibleRepository
             .findBibleByBookAndChapterAndVerse(book, chapter, verse)
             .orElseThrow(RuntimeException::new);
         return BibleConverter.verseFromBible(bible.getBook(), bible.getChapter(), bible.getVerse(),
             bible.getContent());
-    }
-
-    //구간 찾기
-    public SearchSectionResponse getSection(
-        String frontBook,
-        Long frontChapter,
-        Long frontVerse,
-        String backBook,
-        Long backChapter,
-        Long backVerse
-    ){
-        List<Bible> bibleSection = bibleRepository
-            .findBibleSection(
-                frontBook,
-                frontChapter,
-                frontVerse,
-                backBook,
-                backChapter,
-                backVerse
-            );
-        return BibleConverter.sectionFromBible(bibleSection);
     }
 }
