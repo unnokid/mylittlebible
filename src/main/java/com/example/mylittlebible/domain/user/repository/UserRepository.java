10fce2a2f37f13d818fa9a1f49cf0590eae97a51
unnokid <kimki0609@gmail.com>
2022-12-20 22:32:21 +0900

    feat: findByEmail 추가

--- src/main/java/com/example/mylittlebible/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/mylittlebible/domain/user/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.example.mylittlebible.domain.user.repository;
 
 import com.example.mylittlebible.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
