e07888c09fa410d0f62c69b0c76555885fefd5c0
unnokid <kimki0609@gmail.com>
2022-12-20 22:35:34 +0900

    feat: 유니크한 이메일인지 확인 , 타인의 유저 정보보기, 유저 삭제 추가

--- src/main/java/com/example/mylittlebible/domain/user/controller/UserController.java
+++ src/main/java/com/example/mylittlebible/domain/user/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.example.mylittlebible.domain.user.controller;
 
+import com.example.mylittlebible.domain.user.dto.LoginRequest;
 import com.example.mylittlebible.domain.user.dto.SignupRequest;
+import com.example.mylittlebible.domain.user.dto.UserInfoResponse;
 import com.example.mylittlebible.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +24,30 @@ public class UserController {
     }
 
     //회원가입
-    @PostMapping("")
-    public ResponseEntity signUp(@RequestBody SignupRequest signupRequest){
+    @PostMapping("/sign")
+    public ResponseEntity<Void> signUp(@RequestBody SignupRequest signupRequest){
         userService.save(signupRequest);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserInfoResponse> userInfo(@PathVariable Long userId){
+        UserInfoResponse userInfo = userService.getUserInfo(userId);
+
+        return ResponseEntity.ok(userInfo);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId){
+        userService.delete(userId);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{email}")
+    public void validateEmail(@PathVariable String email){
+        userService.validateEmail(email);
     }
 
 }
--- src/main/java/com/example/mylittlebible/domain/user/service/UserService.java
+++ src/main/java/com/example/mylittlebible/domain/user/service/UserService.java
@@ -1,23 +1,66 @@
 package com.example.mylittlebible.domain.user.service;
 
+import com.example.mylittlebible.domain.bible.entity.Bible;
+import com.example.mylittlebible.domain.bible.repository.BibleRepository;
 import com.example.mylittlebible.domain.user.dto.SignupRequest;
+import com.example.mylittlebible.domain.user.dto.UserInfoResponse;
+import com.example.mylittlebible.domain.user.entity.FavoriteId;
+import com.example.mylittlebible.domain.user.entity.User;
 import com.example.mylittlebible.domain.user.repository.UserRepository;
 import com.example.mylittlebible.domain.user.util.UserConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class UserService {
 
     private UserRepository userRepository;
+    private BibleRepository bibleRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+        BibleRepository bibleRepository) {
         this.userRepository = userRepository;
+        this.bibleRepository = bibleRepository;
     }
 
+    @Transactional
     public void save(SignupRequest request){
         validatePassword(request.getPassword(), request.getPasswordCheck());
 
         userRepository.save(UserConverter.toUser(request));
+    }
+
+    @Transactional
+    public void validateEmail(String email){
+        userRepository.findByEmail(email)
+            .ifPresent(a ->{
+                throw new RuntimeException();
+            });
+    }
+
+    @Transactional
+    public UserInfoResponse getUserInfo(Long userId){
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(RuntimeException::new);
+        FavoriteId recent = user.getRecent();
+
+        Bible bible = bibleRepository.findBibleByBookAndChapterAndVerse(
+            recent.getBook(),
+            recent.getChapter(),
+            recent.getVerse()
+        ).orElseThrow(RuntimeException::new);
+
+        return UserConverter.getInfoFromUser(user,bible.getContent());
+    }
+
+    @Transactional
+    public void delete(Long userId){
+        User user = userRepository.findById(userId)
+            .orElseThrow(RuntimeException::new);
+
+        userRepository.delete(user);
     }
 
     private void validatePassword(String password, String passwordCheck){
