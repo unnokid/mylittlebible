5f70b16760c2b2e460a6a8d24e1a0fed06534333
unnokid <kimki0609@gmail.com>
2022-12-22 22:01:52 +0900

    feat: 유저 정보 Converter 추가

--- src/main/java/com/example/mylittlebible/domain/user/util/UserConverter.java
+++ src/main/java/com/example/mylittlebible/domain/user/util/UserConverter.java
@@ -1,5 +1,6 @@
 package com.example.mylittlebible.domain.user.util;
 
+import com.example.mylittlebible.domain.user.dto.MyPageResponse;
 import com.example.mylittlebible.domain.user.dto.SignupRequest;
 import com.example.mylittlebible.domain.user.dto.UserInfoResponse;
 import com.example.mylittlebible.domain.user.entity.User;
@@ -17,13 +18,22 @@ public class UserConverter {
         return user;
     }
 
-    public static UserInfoResponse getInfoFromUser(User user, String content){
+    public static UserInfoResponse getInfoFromUser(User user){
         return UserInfoResponse.builder()
             .name(user.getName())
             .gender(user.getGender())
             .birth(user.getBirth().toString())
             .bookmarkList(user.getBookmark())
-            .recent(content)
+            .build();
+    }
+
+    public static MyPageResponse getMyInfoFromUser(User user){
+        return MyPageResponse.builder()
+            .email(user.getEmail())
+            .name(user.getName())
+            .gender(user.getGender())
+            .birth(user.getBirth().toString())
+            .bookmarkList(user.getBookmark())
             .build();
     }
 
