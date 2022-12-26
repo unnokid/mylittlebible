8f6ade20db6cf69bc29521880a5c01ada06f86ae
unnokid <kimki0609@gmail.com>
2022-12-27 00:03:17 +0900

    feat: 내정보 가져오기 & 유저 정보 가져오기 dto 추가

--- src/main/java/com/example/mylittlebible/domain/user/dto/MyPageResponse.java
+++ src/main/java/com/example/mylittlebible/domain/user/dto/MyPageResponse.java
@@ -3,10 +3,12 @@ package com.example.mylittlebible.domain.user.dto;
 import com.example.mylittlebible.domain.user.entity.Favorite;
 import com.example.mylittlebible.domain.user.entity.Gender;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Builder
