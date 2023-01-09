2b99218c299364163c5e2306661310b389092639
unnokid <kimki0609@gmail.com>
2023-01-10 01:35:13 +0900

    feat: BoardRepository 추가

---
+++ src/main/java/com/example/mylittlebible/domain/board/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.example.mylittlebible.domain.board.repository;
+
+import com.example.mylittlebible.domain.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
